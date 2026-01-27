382677c94fc126330149014b2cc617c970d14c1e
Tero Heinonen <tero.heinonen@alifsemi.com>
2026-01-27 12:35:50 +0200

    Test multiple pdm channels

--- samples/drivers/audio/dmic_alif/src/main.c
+++ samples/drivers/audio/dmic_alif/src/main.c
@@ -1,236 +1,142 @@
-/*
- * Copyright (C) 2024 Alif Semiconductor.
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/audio/dmic.h>
 #include <zephyr/drivers/pdm/pdm_alif.h>
-#include <string.h>
-#include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(PDM, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(test, LOG_LEVEL_DBG);
 
-#define PDM_NODE	DT_ALIAS(pdm_audio)
+uint32_t fir[18] = {0x00000001, 0x00000003, 0x00000003, 0x000007F4, 0x00000004, 0x000007ED,
+		    0x000007F5, 0x000007F4, 0x000007D3, 0x000007FE, 0x000007BC, 0x000007E5,
+		    0x000007D9, 0x00000793, 0x00000029, 0x0000072C, 0x00000072, 0x000002FD};
 
-#define CHANNEL_4  4
-#define CHANNEL_5  5
+#define AUDIO_L       PDM_AUDIO_CHANNEL_4
+#define AUDIO_R       PDM_AUDIO_CHANNEL_5
+#define AUDIO_L_2     PDM_AUDIO_CHANNEL_0
+#define AUDIO_R_2     PDM_AUDIO_CHANNEL_1
+#define PDM_CHANNELS  ((1 << AUDIO_L) | (1 << AUDIO_R) | (1 << AUDIO_L_2) | (1 << AUDIO_R_2))
+#define CHANNEL_COUNT 4
 
-/* PDM Channel 4 configurations */
-#define CH4_PHASE			0x0000001F
-#define CH4_GAIN			0x0000000D
-#define CH4_PEAK_DETECT_TH		0x00060002
-#define CH4_PEAK_DETECT_ITV		0x0004002D
+#define BLOCK_SIZE_BYTES (512 * CHANNEL_COUNT)
+#define BLOCK_COUNT      2
 
-/* PDM Channel 5 configurations */
-#define CH5_PHASE			0x00000003
-#define CH5_GAIN			0x00000013
-#define CH5_PEAK_DETECT_TH		0x00060002
-#define CH5_PEAK_DETECT_ITV		0x00020027
+#define TIMEOUT  250
+#define PDM_MODE PDM_MODE_HIGH_QUALITY_1024_CLK_FRQ
 
-#define SAMPLE_BIT_WIDTH		16
-#define TIMEOUT				5000
+#define PHASE           0x0000001F
+#define GAIN            0x00000600
+#define PEAK_DETECT_TH  0x00060002
+#define PEAK_DETECT_ITV 0x0004002D
+#define IIR_COEF        0x00000004
 
-struct pdm_ch_config pdm_coef_reg;
+K_MEM_SLAB_DEFINE_STATIC(pcm_slab, BLOCK_SIZE_BYTES, BLOCK_COUNT, 4);
 
-uint32_t ch4_fir[18] = {0x00000001, 0x00000003, 0x00000003, 0x000007F4,
-		0x00000004, 0x000007ED, 0x000007F5, 0x000007F4, 0x000007D3,
-		0x000007FE, 0x000007BC, 0x000007E5, 0x000007D9, 0x00000793,
-		0x00000029, 0x0000072C, 0x00000072, 0x000002FD};
-
-/* Channel 5 FIR coefficient */
-uint32_t ch5_fir[18] = {0x00000000, 0x000007FF, 0x00000000, 0x00000004,
-		0x00000004, 0x000007FC, 0x00000000, 0x000007FB, 0x000007E4,
-		0x00000000, 0x0000002B, 0x00000009, 0x00000016, 0x00000049,
-		0x00000793, 0x000006F8, 0x00000045, 0x00000178};
-
-/* The number of channels tested - changes with the test case */
-#define NUM_CHANNELS	2
-/*
- * Driver will allocate blocks from this slab to receive audio data into them.
- * Application, after getting a given block from the driver and processing its
- * data, needs to free that block.
- */
-#define PCMJ_BLOCK_SIZE		30000
-
-/* Number of blocks in the slab */
-#define MEM_SLAB_NUM_BLOCKS		2
-
-/* size of buffer where the whole data is stored */
-#define DATA_SIZE	(PCMJ_BLOCK_SIZE * MEM_SLAB_NUM_BLOCKS)
-
-static uint8_t pcmj_data[DATA_SIZE];
-
-K_MEM_SLAB_DEFINE(mem_slab, PCMJ_BLOCK_SIZE, MEM_SLAB_NUM_BLOCKS, 4);
-
-/*
- * The list of channels to test
- * The number of channels should match the PDM_CHANNELS
- */
-#define PDM_CHANNELS	PDM_MASK_CHANNEL_4 | PDM_MASK_CHANNEL_5
-
-void pdm_ch_config(void)
+void pdm_ch_config(const struct device *pcmj_device)
 {
-	const struct device *pcmj_device = DEVICE_DT_GET(PDM_NODE);
+	struct pdm_ch_config pdm_coef_reg;
 
-	pdm_set_ch_phase(pcmj_device, CHANNEL_4, CH4_PHASE);
-
-	pdm_set_ch_gain(pcmj_device, CHANNEL_4, CH4_GAIN);
-
-	pdm_set_peak_detect_th(pcmj_device, CHANNEL_4, CH4_PEAK_DETECT_TH);
-
-	pdm_set_peak_detect_itv(pcmj_device, CHANNEL_4, CH4_PEAK_DETECT_ITV);
-
-	pdm_coef_reg.ch_num  = 4;
-
-	/* Channel 4 fir coefficient */
-	memcpy(pdm_coef_reg.ch_fir_coef, ch4_fir, sizeof(pdm_coef_reg.ch_fir_coef));
-
-	/* Channel IIR Filter Coefficient */
-	pdm_coef_reg.ch_iir_coef	= 0x00000004;
-
+	pdm_set_ch_phase(pcmj_device, AUDIO_L, PHASE);
+	pdm_set_ch_gain(pcmj_device, AUDIO_L, GAIN);
+	pdm_coef_reg.ch_num = AUDIO_L;
+	memcpy(pdm_coef_reg.ch_fir_coef, fir, sizeof(pdm_coef_reg.ch_fir_coef));
+	pdm_coef_reg.ch_iir_coef = IIR_COEF;
 	pdm_channel_config(pcmj_device, &pdm_coef_reg);
 
-	pdm_set_ch_gain(pcmj_device, CHANNEL_5, CH5_GAIN);
-
-	pdm_set_ch_phase(pcmj_device, CHANNEL_5, CH5_PHASE);
-
-	pdm_set_peak_detect_th(pcmj_device, CHANNEL_5, CH5_PEAK_DETECT_TH);
-
-	pdm_set_peak_detect_itv(pcmj_device, CHANNEL_5, CH5_PEAK_DETECT_ITV);
-
-	pdm_coef_reg.ch_num  = 5;
-
-	/* Channel 5 fir coefficient */
-	memcpy(pdm_coef_reg.ch_fir_coef, ch5_fir, sizeof(pdm_coef_reg.ch_fir_coef));
-
-	/* Channel IIR Filter Coefficient */
-	pdm_coef_reg.ch_iir_coef	= 0x00000004;
-
+	pdm_set_ch_gain(pcmj_device, AUDIO_R, GAIN);
+	pdm_set_ch_phase(pcmj_device, AUDIO_R, PHASE);
+	pdm_coef_reg.ch_num = AUDIO_R;
+	memcpy(pdm_coef_reg.ch_fir_coef, fir, sizeof(pdm_coef_reg.ch_fir_coef));
+	pdm_coef_reg.ch_iir_coef = IIR_COEF;
 	pdm_channel_config(pcmj_device, &pdm_coef_reg);
 
-	pdm_mode(pcmj_device, PDM_MODE_STANDARD_VOICE_512_CLK_FRQ);
-}
+	pdm_set_ch_gain(pcmj_device, AUDIO_L_2, GAIN);
+	pdm_set_ch_phase(pcmj_device, AUDIO_L_2, PHASE);
+	pdm_coef_reg.ch_num = AUDIO_L_2;
+	memcpy(pdm_coef_reg.ch_fir_coef, fir, sizeof(pdm_coef_reg.ch_fir_coef));
+	pdm_coef_reg.ch_iir_coef = IIR_COEF;
+	pdm_channel_config(pcmj_device, &pdm_coef_reg);
 
-static int config_channel(const struct device *dmic_dev,
-				struct dmic_cfg *cfg, uint8_t block_count)
-{
-	int rc;
-	int k = 0;
-	int i = 0;
-	uint32_t data;
+	pdm_set_ch_gain(pcmj_device, AUDIO_R_2, GAIN);
+	pdm_set_ch_phase(pcmj_device, AUDIO_R_2, PHASE);
+	pdm_coef_reg.ch_num = AUDIO_R_2;
+	memcpy(pdm_coef_reg.ch_fir_coef, fir, sizeof(pdm_coef_reg.ch_fir_coef));
+	pdm_coef_reg.ch_iir_coef = IIR_COEF;
+	pdm_channel_config(pcmj_device, &pdm_coef_reg);
 
-	if (dmic_dev == NULL || cfg == NULL) {
-		LOG_ERR("%s: input invalid\n", __func__);
-		return -1;
-	}
-
-	dmic_configure(dmic_dev, cfg);
-
-	pdm_ch_config();
-
-	printk("Start Speaking or Play some Audio!\n");
-
-	rc = dmic_trigger(dmic_dev, DMIC_TRIGGER_START);
-	if (rc < 0) {
-		LOG_ERR("dmic_trigger error\n");
-		return rc;
-	}
-
-	void *buffer;
-
-	k = 0;
-
-	for (i = 0; i < block_count; ++i) {
-		rc = dmic_read(dmic_dev, 0, &buffer, &data, TIMEOUT);
-		if (rc < 0) {
-			dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
-			return rc;
-		}
-
-		/* copy the data from the buffer to the pcmj data */
-		if (k + data <= DATA_SIZE) {
-			memcpy(pcmj_data + k, buffer, data);
-			k += data;
-		}
-
-		k_mem_slab_free(&mem_slab, buffer);
-	}
-
-	printk("Stop recording\n");
-	LOG_INF("PCM samples will be stored in %p address and size of "
-	"buffer is %d\n", (void *)pcmj_data, sizeof(pcmj_data));
-	LOG_INF("Block freed at address: %p\n", (void *)buffer);
-
-	rc = dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
-	return rc;
-}
-
-void print_data(void)
-{
-	int32_t i;
-	/*
-	 * print the pointer where the data is stored and first 80 bytes of data
-	 */
-	LOG_INF("pcm data : %p\n", (void *)pcmj_data);
-
-	for (i = 0; i < 80; i += 8) {
-		LOG_INF(" %x %x %x %x %x %x %x %x\n", pcmj_data[i], pcmj_data[i+1],
-		pcmj_data[i+2], pcmj_data[i+3], pcmj_data[i+4], pcmj_data[i+5],
-		pcmj_data[i+6], pcmj_data[i+7]);
-	} k_msleep(500);
-}
-
-void set_config(struct dmic_cfg *cfg, struct pcm_stream_cfg *stream)
-{
-	uint32_t channel_map = 0;
-
-	stream->pcm_width = SAMPLE_BIT_WIDTH;
-	cfg->streams = stream;
-	cfg->streams[0].mem_slab = &mem_slab;
-	cfg->channel.req_num_streams = 1;
-	cfg->channel.req_num_chan = NUM_CHANNELS;
-	cfg->streams[0].block_size = PCMJ_BLOCK_SIZE;
-
-	channel_map = PDM_CHANNELS;
-
-	cfg->channel.req_chan_map_lo = channel_map;
-
-	LOG_INF("memslab: %p\n", cfg->streams[0].mem_slab);
-	LOG_INF("channel_map %x block size: %x\n", channel_map, PCMJ_BLOCK_SIZE);
-}
-
-void init_pdm(void)
-{
-	const struct device *pcmj_device;
-	struct dmic_cfg cfg;
-	struct pcm_stream_cfg stream;
-
-	LOG_INF("PDM init okay\n");
-
-	pcmj_device = DEVICE_DT_GET(PDM_NODE);
-
-	if (!pcmj_device) {
-		LOG_ERR("pcmj_device not found\n");
-		return;
-	}
-
-	if (!device_is_ready(pcmj_device)) {
-		LOG_ERR("device not ready\n");
-		return;
-	}
-
-	set_config(&cfg, &stream);
-
-	config_channel(pcmj_device, &cfg, MEM_SLAB_NUM_BLOCKS);
-
-	print_data();
+	pdm_mode(pcmj_device, PDM_MODE);
 }
 
 int main(void)
 {
-	init_pdm();
+	printk("Start test\n");
+
+	const struct device *dmic = DEVICE_DT_GET(DT_NODELABEL(lppdm));
+	if (!device_is_ready(dmic)) {
+		LOG_ERR("DMIC/PDM device not ready");
+		return 0;
+	}
+
+	struct dmic_cfg cfg = {0};
+	cfg.channel.req_num_chan = CHANNEL_COUNT;
+	cfg.channel.req_chan_map_lo = PDM_CHANNELS;
+
+	static struct pcm_stream_cfg stream_cfg;
+	stream_cfg.block_size = BLOCK_SIZE_BYTES;
+	stream_cfg.mem_slab = &pcm_slab;
+	cfg.streams = &stream_cfg;
+
+	int ret = dmic_configure(dmic, &cfg);
+	if (ret) {
+		LOG_ERR("dmic_configure failed: %d", ret);
+		return 0;
+	}
+	pdm_ch_config(dmic);
+
+	ret = dmic_trigger(dmic, DMIC_TRIGGER_START);
+	if (ret) {
+		LOG_ERR("DMIC_TRIGGER_START failed: %d", ret);
+		return 0;
+	}
+
+	while (1) {
+		void *buffer = NULL;
+		size_t size = 0;
+
+		ret = dmic_read(dmic, 0, &buffer, &size, TIMEOUT);
+
+		if (ret) {
+			LOG_ERR("Data read fail %d", ret);
+			break;
+		}
+
+		const int16_t *sample_ptr = (const int16_t *)buffer;
+		size_t sample_cnt = size / sizeof(int16_t);
+
+		int16_t max_a = 0;
+		int16_t max_b = 0;
+		int16_t max_c = 0;
+		int16_t max_d = 0;
+
+		for (size_t i = 0; i + 1 < sample_cnt; i += 4) {
+			if (sample_ptr[i] > max_a) {
+				max_a = sample_ptr[i];
+			}
+			if (sample_ptr[i + 1] > max_b) {
+				max_b = sample_ptr[i + 1];
+			}
+			if (sample_ptr[i + 2] > max_c) {
+				max_c = sample_ptr[i + 2];
+			}
+			if (sample_ptr[i + 3] > max_d) {
+				max_d = sample_ptr[i + 3];
+			}
+		}
+
+		LOG_INF("peak%5d peak%5d peak%5d peak%5d", max_a, max_b, max_c, max_d);
+		k_mem_slab_free(&pcm_slab, buffer);
+	}
+
+	(void)dmic_trigger(dmic, DMIC_TRIGGER_STOP);
 
 	return 0;
 }
